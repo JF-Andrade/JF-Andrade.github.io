72abc2cb73725e91bfb8ffe4b8982a5a5629f889
Jordão Fernandes de Andrade <j230241@dac.unicamp.br>
2025-08-24 00:20:19 -0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Este arquivo é para qualquer funcionalidade interativa ou JavaScript personalizado.
+// Por exemplo:
+// - Validação de formulários
+// - Animações mais complexas
+// - Requisições a APIs
+// Você pode adicionar seu código aqui para interagir com o site.
+
+console.log('O arquivo main.js foi carregado com sucesso!');
